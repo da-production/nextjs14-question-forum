f67a3e4e84c5803ca9795c93c1d62dff9be302e3
MEBROUKI Amine <ami.mebrouki@cnac.local>
2024-02-22 14:42:43 +0100

    update userauthform

--- app/Components/UserAuthForm.tsx
+++ app/Components/UserAuthForm.tsx
@@ -1,13 +1,25 @@
-import { FC } from 'react'
+"use client"
+import { FC, useState } from 'react'
 import { Button } from './ui/Button'
 import { cn } from '@/lib/utils'
+import { signIn } from "next-auth/react"
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+    const [isLoading,setIsLoading] = useState<boolean>(false)
+
+    const loginWithGoogle = async () => {
+        setIsLoading(true)
+        try{
+            await signIn('google')
+        }catch(error){
+            console.log(error)
+        }
+    }
     return (
         <div className={cn('flex justify-center', className)} {...props}>
-            <Button>Google</Button>
+            <Button size='sm' className='w-full' onClick={loginWithGoogle}>Google</Button>
         </div>
     )
 }
